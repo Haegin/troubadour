b0f00578f4c7de583806739fdb7477d514bd3b4b
Harry Mills <harry@freeagent.com>
2016-01-20 22:35:08 -0500

    Use FastClick to support fast taps on iOS

--- fe/application.jsx
+++ fe/application.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route } from 'react-router';
+import attachFastClick from 'fastclick';
+
 import App from './components/app.jsx';
 
+attachFastClick(document.body);
 render((
   <Router>
     <Route path="/" component={App}></Route>
