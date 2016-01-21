7ab161b494d9f12fd5b6d99c088ab9a80f0f4b3e
Harry Mills <harry@freeagent.com>
2016-01-20 22:36:46 -0500

    Use FastClick correctly

--- fe/application.jsx
+++ fe/application.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route } from 'react-router';
-import attachFastClick from 'fastclick';
+import FastClick from 'fastclick';
 
 import App from './components/app.jsx';
 
-attachFastClick(document.body);
+FastClick.attach(document.body);
 render((
   <Router>
     <Route path="/" component={App}></Route>
